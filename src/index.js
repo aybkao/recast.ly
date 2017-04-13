dcf3793ac3acd71acd80cd314358e21deaf69cdd
Hack Reactor Student <student@hackreactor.com>
2017-04-12 22:13:49 -0700

    PAsses tests up to app, click function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 // TODO: Render the `App` component to the DOM
 
+ReactDOM.render(<App />, document.getElementById("app"));
+
+
+// sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9pbmRleC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSIsImZpbGUiOiJpbmRleC5qcyIsInNvdXJjZXNDb250ZW50IjpbIi8vIFRPRE86IFJlbmRlciB0aGUgYEFwcGAgY29tcG9uZW50IHRvIHRoZSBET01cblxuIl19
