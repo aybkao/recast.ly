976cd5bd301cb1c913715e3a5d9548377d0d3091
Hack Reactor Student <student@hackreactor.com>
2017-04-12 23:03:03 -0700

    Working towards button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,37 @@
 class App extends React.Component {
-	constructor (){
-		super();
-		this.state = {
-			data: window.exampleVideoData,
-			video: window.exampleVideoData[0]
-		}
-	}
-	render (){
-		return ( 
-	  <div>
-	    <Nav />
-	    <div className="col-md-7">
-	      <VideoPlayer video={this.state.video}/>
-	    </div>
-	    <div className="col-md-5">
-	      <VideoList videos={this.state.data}/>
-	    </div>
-	  </div>
-    )
-	}
+  constructor () { 
+    super();
+    this.state = {
+      data: window.exampleVideoData,
+      video: window.exampleVideoData[0]
+    };
+   onClick={this.handleClick.bind(this)}
+  }  
+
+  handleClick () {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  
+  
+
+
+  render () {
+    if ( )
+    return ( 
+    <div>
+      <Nav />
+      <div className="col-md-7">
+        <VideoPlayer video={this.state.video} />
+      </div>
+      <div className="col-md-5">
+        <VideoList videos={this.state.data} />
+      </div>
+    </div>
+    );
+  }
 }
 
 
