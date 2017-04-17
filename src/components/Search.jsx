a83d8d3849d4bc46dd5efe7aa5416d16f0447148
aybkao <aybkao@gmail.com>
2017-04-17 02:19:54 -0700

    functional stateless search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,52 +1,52 @@
 
 
-// var Search = (props) => (
-//   <div className="search-bar form-inline">
-//     <input
-//       className="form-control"
-//       type="text"
-//       onkeydown={props.handleSearchInput(this)}
-//       placeholder="Search......"
-//     />
-//     <button className="btn hidden-sm-down">
-//       <span className="glyphicon glyphicon-search"></span>
-//     </button>
-//   </div>
-// )
+var Search = (props) => (
+  <div className="search-bar form-inline">
+    <input
+      className="form-control"
+      type="text"
+      onChange={(e) => props.handleSearchInput(e.target.value)}
+      placeholder="Search......"
+    />
+    <button className="btn hidden-sm-down">
+      <span className="glyphicon glyphicon-search"></span>
+    </button>
+  </div>
+)
 
 
-class Search extends React.Component {
-  constructor(props) {
-    super(props);
+// class Search extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      value: ''
-    };
-  }
+//     this.state = {
+//       value: ''
+//     };
+//   }
 
-  handleInputChange(e) {
-    this.props.handleSearchInput(e.target.value);
-    this.setState({
-      value: e.target.value
-    });
-  }
+//   handleInputChange(e) {
+//     this.props.handleSearchInput(e.target.value);
+//     this.setState({
+//       value: e.target.value
+//     });
+//   }
 
-  render() {
-    return (
-      <div className="search-bar form-inline">
-        <input
-          className="form-control"
-          type="text"
-          value={this.state.value}
-          onChange={this.handleInputChange.bind(this)}
-        />
-        <button className="btn hidden-sm-down">
-          <span className="glyphicon glyphicon-search"></span>
-        </button>
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div className="search-bar form-inline">
+//         <input
+//           className="form-control"
+//           type="text"
+//           value={this.state.value}
+//           onChange={this.handleInputChange.bind(this)}
+//         />
+//         <button className="btn hidden-sm-down">
+//           <span className="glyphicon glyphicon-search"></span>
+//         </button>
+//       </div>
+//     );
+//   }
+// }
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
